9acf6861a1640f07499a81070cb9acd01724c485
Leonardo Godoy <leonardo.godoy.2009@outlook.com>
2023-06-08 15:27:39 -0400

    Reordered Main file

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-from utils.Exceptions import *
-from utils.helper import *
-from shodan import Shodan
+#!/usr/bin/env python3
+
+#Import common libraries
 import requests
 import urllib3
 import json
 
+#Import external libraries
+from shodan import Shodan
+
+#Import custom classes and functions
+from utils.Exceptions import *
+from utils.helper import *
+
+#Set Disable Warnings for HTTPS URL's
 urllib3.disable_warnings()
 
+#Set Logger
+LOGGER = logger("SHODAN_LOGGER")
+
+#Get configurations from config file
 config = read_config()
 
 SPLUNK_URL = config["default"]["URL"]
 API_KEY = config["default"]["API_KEY"]
 HEC_TOKEN = config["default"]["HEC_TOKEN"]
-LOGGER = logger("SHODAN_LOGGER")
 
+
+#Set Authorization Header for Splunk
+headers = {
+    "Authorization": f"Splunk {HEC_TOKEN}"
+}
+
+#Base logic
 try:
     LOGGER.info('Starting Shodan Stream')
     api = Shodan(API_KEY)
@@ -34,9 +52,7 @@ try:
                 except:
                     pass
             LOGGER.info("Shodan Banner is ready")
-            headers = {
-                "Authorization": "Splunk "+HEC_TOKEN
-            }
+            
             data = json.dumps({'host':'ShodanAPI','sourcetype':'shodan_monitoring','source':'ShodanAPI','event':banner})
             response = requests.post(url=SPLUNK_URL, headers=headers, data=data, verify=False)
             print(response.status_code)
